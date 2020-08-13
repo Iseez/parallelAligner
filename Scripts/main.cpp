4543c610e964adf168b196d355156495685068b7
Iseez <iesedeno@gmail.com>
2020-08-13 00:16:06 -0500

    updates

--- Scripts/main.cpp
+++ Scripts/main.cpp
@@ -1,5 +1,6 @@
 #include "reader.h"
 #include "comparison.h"
+#include <time.h>
 
 int main(int argc, char const *argv[]) {
   string file = argv[1];//"/home/iseez/Documents/Paralelo/alignment/Data/example.fasta";
@@ -11,12 +12,14 @@ int main(int argc, char const *argv[]) {
   comparison objComp;
   //float kd = objComp.kmdist(objFasta.reads->at(0),objFasta.reads->at(1),K);
   float kd;
+  clock_t startS = clock();
   for(int i = 0;i < len;i++){
-    for(int j = 0; j < len; len++){
+    for(int j = 0; j < len; j++){
       kd = objComp.kmdist(objFasta.reads->at(i),objFasta.reads->at(j),K);
-      if(kd < 0.1) printf("--\n");
     }
   }
+  clock_t endS = clock();
+  printf("Elapsed Time from serial implementation:%f seconds\n", double(endS-startS)/double (CLOCKS_PER_SEC));
   printf("%f\n", kd);
   std::cin.get();
   return 0;
